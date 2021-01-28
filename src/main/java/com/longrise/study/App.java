0417b805e29ffb578212a60b2b1f37f32573a8ff
ashesoftime <gongjwen@foxmail.com>
2021-01-28 11:57:21 +0800

    update

--- src/main/java/com/longrise/study/App.java
+++ src/main/java/com/longrise/study/App.java
@@ -1,5 +1,6 @@
 package com.longrise.study;
 
+import java.util.UUID;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
@@ -8,22 +9,31 @@ import java.util.concurrent.ConcurrentLinkedQueue;
  */
 public class App {
     public static void main(String[] args) {
-        ConcurrentLinkedQueue<Object> queue = new ConcurrentLinkedQueue<>();
-        queue.add(new Object());
-        Object object = new Object();
-        int loops = 0;
-        Runtime runtime = Runtime.getRuntime();
-        long last = System.currentTimeMillis();
-        while (true) {
-            if(loops % 10000 == 0){
-                long now = System.currentTimeMillis();
-                long duration = now - last;
-                last = now;
-                System.out.println(String.format("duration=%d queue.size=%d memory max=%d free=%d total=%d%n", duration, queue.size(), runtime.maxMemory(), runtime.freeMemory(), runtime.totalMemory()));
-            }
-            queue.add(object);
-            queue.remove(object);
-            ++loops;
+        // ConcurrentLinkedQueue<Object> queue = new ConcurrentLinkedQueue<>();
+        // queue.add(new Object());
+        // Object object = new Object();
+        // int loops = 0;
+        // Runtime runtime = Runtime.getRuntime();
+        // long last = System.currentTimeMillis();
+        // while (true) {
+        //     if(loops % 10000 == 0){
+        //         long now = System.currentTimeMillis();
+        //         long duration = now - last;
+        //         last = now;
+        //         System.out.println(String.format("duration=%d queue.size=%d memory max=%d free=%d total=%d%n", duration, queue.size(), runtime.maxMemory(), runtime.freeMemory(), runtime.totalMemory()));
+        //     }
+        //     queue.add(object);
+        //     queue.remove(object);
+        //     ++loops;
+        // }
+        ss();
+    }
+
+    public static void ss(){
+        for (int i = 1; i <= 108; i++) {
+            
+            String ss = UUID.randomUUID().toString().replaceAll("-", "");
+            System.out.println(ss);
         }
     }
 }
---
+++ src/main/java/com/longrise/study/ArrayDemo.java
@@ -0,0 +1,8 @@
+package com.longrise.study;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        String str1 = "3,2,1,4,5,6,7,8,9";
+        System.out.println(str1);
+    }
+}
