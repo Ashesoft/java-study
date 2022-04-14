d83538de9be33ab167642b3212d8e1c72c5e9716
Ashesoft <gongjwen@foxmail.com>
2022-04-14 11:24:39 +0800

    update

--- src/main/java/com/longrise/cache/GuavaCacheDemo.java
+++ src/main/java/com/longrise/cache/GuavaCacheDemo.java
@@ -1,9 +1,20 @@
 package com.longrise.cache;
 
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 
 public class GuavaCacheDemo {
   public static void main(String[] args) {
-    CacheBuilder.newBuilder().concurrencyLevel(Runtime.getRuntime().availableProcessors()).build();
+    // CacheBuilder.newBuilder().concurrencyLevel(Runtime.getRuntime().availableProcessors()).build();
+    t1();
+  }
+
+  private static void t1() {
+    Cache<String, String> cache = CacheBuilder.newBuilder().expireAfterAccess(3, TimeUnit.MINUTES).build();
+    cache.put("word", "Hello Guava Cache");
+    System.out.println("GuavaCacheDemo.t1()");
+    System.out.println(cache.getIfPresent("word1"));
   }
 }
